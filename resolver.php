f3018ee53567a57df66055ccb96ec362275ea11c
LeoHugues <leo.hugues@hotmail.fr>
2016-05-25 11:58:40 +0200

    finission

--- resolver.php
+++ resolver.php
@@ -1,38 +1,113 @@
 <?php
+
+main();
+
 /**
- * Created by PhpStorm.
- * User: leo
- * Date: 23/05/2016
- * Time: 18:13
+ * Le programme
  */
+function main() {
+    $grilleDifficile =
+        [
+            [0,9,2,  0,0,0,  0,8,0],
+            [0,7,0,  3,0,0,  0,0,0],
+            [4,0,3,  6,0,8,  2,7,0],
 
-$resolver = new resolver();
+            [0,4,0,  1,0,0,  0,0,7],
+            [0,0,0,  5,0,2,  0,0,0],
+            [3,0,0,  0,0,4,  0,5,0],
 
-$resolver->main();
+            [0,1,6,  7,0,3,  5,0,8],
+            [0,0,0,  0,0,9,  0,1,0],
+            [0,3,0,  0,0,0,  6,9,0]
+        ];
+    $grilleDiabolique =
+        [
+            [0,6,0,  4,7,0,  0,0,0],
+            [2,0,8,  3,0,0,  0,9,0],
+            [0,1,3,  0,0,5,  0,4,0],
 
-class resolver {
+            [0,0,0,  0,9,0,  2,0,0],
+            [6,0,0,  0,0,0,  0,0,3],
+            [0,0,7,  0,1,0,  0,0,0],
+
+            [0,8,0,  1,0,0,  4,7,0],
+            [0,3,0,  0,0,7,  8,0,1],
+            [0,0,0,  0,4,8,  0,2,0]
+        ];
+
+    $grilleDifficilePourResolver =
+        [
+            [9,0,0,  1,0,0,  0,0,5],
+            [0,0,5,  0,9,0,  2,0,1],
+            [8,0,0,  0,4,0,  0,0,0],
+
+            [0,0,0,  0,8,0,  0,0,0],
+            [0,0,0,  7,0,0,  0,0,0],
+            [0,0,0,  0,2,6,  0,0,9],
+
+            [2,0,0,  3,0,0,  0,0,6],
+            [0,0,0,  2,0,0,  9,0,0],
+            [0,0,1,  9,0,4,  5,7,0]
+        ];
+
+    $pireDesGrilles =
+        [
+            [0,0,0,  0,0,0,  0,0,0],
+            [0,0,0,  0,0,3,  0,8,5],
+            [0,0,1,  0,2,0,  0,0,0],
+
+            [0,0,0,  5,0,7,  0,0,0],
+            [0,0,4,  0,0,0,  1,0,0],
+            [0,9,0,  0,0,0,  0,0,0],
+
+            [5,0,0,  0,0,0,  0,7,3],
+            [0,0,2,  0,1,0,  0,0,0],
+            [0,0,0,  0,4,0,  0,0,9]
+        ];
+
+    $resolver = new Resolver();
+
+    echo "Grille a resoudre :<br><br>";
+    $resolver->affichage($pireDesGrilles);
+
+    $resolver->resolve($pireDesGrilles);
+    echo "Grille resolue :<br><br>";
+    $resolver->affichage($resolver->grilleResolu);
+
+    echo '<br> Nombre d\'iteration pour resoudre la grille : ' . $resolver->itération;
+}
+
+
+/**
+ * Class Resolver
+ *
+ * classe qui contient toutes les méthodes pour résoudre une grille de sudoku
+ */
+class Resolver {
      public $grilleResolu = array();
+     public $itération = 0;
 
-    // Fonction d'affichage
     /**
-     * @param $grille array
+     * Fonction d'affichage
      */
     function affichage ($grille)
     {
         for ($i=0; $i<9; $i++)
         {
             for ($j=0; $j<9; $j++) {
-                printf((($j + 1) % 3) ? "%d " : "%d|", $grille[$i][$j]);
+                printf((($j + 1) % 3) ? "%d " : "%d | ", $grille[$i][$j]);
             }
             echo('<br>');
             if (!(($i +1)%3)) {
-                echo("------------------<br>");
+                echo("-----------------------<br>");
             }
         }
         echo("<br><br>");
     }
 
     /**
+     * Teste la présence d'un chiffre sur une ligne
+     *
      * @param $k int Nombre recherché
      * @param $grille array grille de sudoku
      * @param $i int position de la ligne
@@ -49,6 +124,8 @@ class resolver {
     }
 
     /**
+     * Teste la présence d'un chiffre sur une colone
+     *
      * @param $k int Nombre recherché
      * @param $grille array grille de sudoku
      * @param $j int position de la colone
@@ -66,6 +143,8 @@ class resolver {
     }
 
     /**
+     * Teste la présence d'un chiffre dans un bloc
+     *
      * @param $k int Nombre recherché
      * @param $grille array grille de sudoku
      * @param $i int position de la ligne
@@ -74,7 +153,7 @@ class resolver {
      */
     function absentSurBloc ($k, $grille, $i, $j)
     {
-        // Cela permet de retrouver les coordonnée de la première case du bloc
+        // Cela permet de retrouver les coordonnées de la première case du bloc
         $_i = $i-($i%3);
         $_j = $j-($j%3);  // ou encore : _i = 3*(i/3), _j = 3*(j/3);
         for ($i=$_i; $i < $_i+3; $i++) {
@@ -88,18 +167,18 @@ class resolver {
         return true;
     }
 
-    function estValid($grille) {
-        for($i = 0; $i < 8; $i++) {
-            for($j = 0; $j < 8;$i ++) {
-                if ($grille[$i][$j] == 0) {
-
-                }
-            }
-        }
-    }
-
+    /**
+     * Fonction récursive qui va résoudre le sudoku
+     *
+     * @param $grille
+     * @param $position
+     * @return bool
+     */
     function estValide ($grille, $position)
     {
+        // On incrémente le nombre d'itération à chaque fois que l'on passe dans la fonction
+        $this->itération++;
+
         // Si on est à la 82e case (on sort du tableau)
         if ($position == 9*9) {
             return true;
@@ -122,7 +201,7 @@ class resolver {
             if ($this->absentSurLigne($k,$grille,$i) && $this->absentSurColonne($k,$grille,$j) && $this->absentSurBloc($k,$grille,$i,$j)) {
                 // On enregistre k dans la grille
                 $grille[$i][$j] = $k;
-                $this->grilleResolu = $grille;
+                $this->grilleResolu[$i][$j] = $k;
 
                 // On appelle récursivement la fonction estValide(), pour voir si ce choix est bon par la suite
                 if ( $this->estValide ($grille, $position+1) ) {
@@ -137,28 +216,12 @@ class resolver {
         return false;
     }
 
-    function main ()
-    {
-        $grille =
-            [
-                [9,0,0,1,0,0,0,0,5],
-                [0,0,5,0,9,0,2,0,1],
-                [8,0,0,0,4,0,0,0,0],
-                [0,0,0,0,8,0,0,0,0],
-                [0,0,0,7,0,0,0,0,0],
-                [0,0,0,0,2,6,0,0,9],
-                [2,0,0,3,0,0,0,0,6],
-                [0,0,0,2,0,0,9,0,0],
-                [0,0,1,9,0,4,5,7,0]
-            ];
-
-        printf("Grille avant <br>");
-        $this->affichage($grille);
-
-        $this->estValide($grille,0);
-
-        printf("Grille apres<br>");
-        $this->affichage($this->grilleResolu);
+    /**
+     * @param $grille
+     */
+    public function resolve($grille) {
+        $this->grilleResolu = $grille;
+        $this->itération = 0;
+        $this->estValide($grille, 0);
     }
-
 }
